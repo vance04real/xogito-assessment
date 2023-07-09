9157532f96a786d7c49cf5fedc3bd134c015e396
Evans K F Chikuni <vance04real@gmail.com>
2023-07-10 00:26:02 +0200

    setting up unit tests

---
+++ src/main/java/com/xogito/projectmanagement/XogitoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.xogito.projectmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XogitoAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XogitoAssessmentApplication.class, args);
+    }
+
+}
--- src/test/java/com/xogito/projectmanagement/service/ProjectServiceImplUTest.java
+++ src/test/java/com/xogito/projectmanagement/service/ProjectServiceImplUTest.java
@@ -1,6 +1,5 @@
 package com.xogito.projectmanagement.service;
 
-
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +8,5 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(MockitoExtension.class)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-public class UserServiceImplUTest {
+public class ProjectServiceImplUTest {
 }
