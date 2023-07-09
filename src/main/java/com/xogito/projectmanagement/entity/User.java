042d88b5c3b5e308bd377f26902fa65a908e3b59
Evans K F Chikuni <vance04real@gmail.com>
2023-07-10 00:30:35 +0200

    create user entity

---
+++ src/main/java/com/xogito/projectmanagement/entity/User.java
@@ -0,0 +1,32 @@
+package com.xogito.projectmanagement.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_project",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "project_id")
+    )
+    private List<Project> assignedProjects;
+}
