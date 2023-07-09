4a147072f8209a81eda4be9d82b8db43f472dcc1
Evans K F Chikuni <vance04real@gmail.com>
2023-07-10 00:39:51 +0200

    create project entity and adding dependencies for openapi documentation

--- src/main/java/com/xogito/projectmanagement/entity/Project.java
+++ src/main/java/com/xogito/projectmanagement/entity/Project.java
@@ -1,2 +1,34 @@
-package com.xogito.projectmanagement.entity;public class Project {
+package com.xogito.projectmanagement.entity;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @NotNull
+    private String name;
+    private String description;
+    @ManyToMany(mappedBy = "assignedProjects")
+    private List<User> assignedUsers;
+
 }
